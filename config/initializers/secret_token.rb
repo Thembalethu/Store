d01d56d89bbf3351ff6cdb2f7e9d61c4ef3412fb
Thembalethu <thembalethu.zintwana@gmail.com>
2013-12-26 14:20:02 +0200

    Store Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'c86c395002aad1c8a052b062fe7188d9642d02e4f2553d6f33c87a924b486edc38738e5774194219b330fb2db98b11022c05e18e483afa935f10e3b86a58e430'
